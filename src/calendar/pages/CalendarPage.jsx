b543483a38474d586404375bcbb4e7674084308b
ernesto2011 <skillmx107@gmail.com>
2024-08-02 00:26:45 -0600

    :lipstick: bigcalendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,41 @@
-import { NavBar } from "../"
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import enUS from "date-fns/locale/en-US";
+import { NavBar } from "../";
+import { addHours, parse, format, startOfWeek, getDay } from "date-fns";
 
+const locales = {
+  "en-US": enUS,
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+const events = [{
+    title: "Meeting",
+    start: new Date(),
+    end: addHours (new Date(), 2),
+    bgColor: '#fafafa',
+    user:{
+        _id: "123",
+        name: "John Doe",
+    }
+  }, {
+}]
 export const CalendarPage = () => {
   return (
     <>
-    <NavBar/>
+      <NavBar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px' }}
+      />
     </>
-  )
-}
+  );
+};
