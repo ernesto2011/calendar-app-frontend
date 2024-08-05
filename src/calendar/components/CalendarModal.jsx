25ed8a44ba78823dec6dd6b1833429e11fab00c5
ernesto2011 <skillmx107@gmail.com>
2024-08-04 19:21:13 -0600

    form para calendar

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,37 +1,90 @@
+import { addHours } from "date-fns";
 import { useState } from "react";
-import Modal from "react-modal"
+import Modal from "react-modal";
 
 const customStyles = {
-    content: {
-      top: '50%',
-      left: '50%',
-      right: 'auto',
-      bottom: 'auto',
-      marginRight: '-50%',
-      transform: 'translate(-50%, -50%)',
-    },
-}
-Modal.setAppElement('#root');
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+Modal.setAppElement("#root");
 
 export const CalendarModal = () => {
-    const [isOpenModal, setIsOpenModal] = useState(true)
-    const closeModal = () => {
-      setIsOpenModal(false)
-      
-    }
+  const [isOpenModal, setIsOpenModal] = useState(true);
+  const [formValues, setformValues] = useState({
+    title: "Ernesto",
+    notes: 'Lopez',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+  })
+  const handleInputChange = ({target}) => {
+    setformValues({...formValues, [target.name]: target.value });
+  };
+  const closeModal = () => {
+    setIsOpenModal(false);
+  };
 
   return (
     <Modal
-    isOpen={isOpenModal}
-    onRequestClose={closeModal}
-    style={customStyles}
-    className="modal"
-    overlayClassName='modal-fondo'
-    closeTimeoutMS={250}
+      isOpen={isOpenModal}
+      onRequestClose={closeModal}
+      style={customStyles}
+      className="modal"
+      overlayClassName="modal-fondo"
+      closeTimeoutMS={250}
     >
-        <h1>Hola Mundo</h1>
+      <h1> Nuevo evento </h1>
+      <hr />
+      <form className="container">
+        <div className="form-group mb-2">
+          <label>Fecha y hora inicio</label>
+          <input className="form-control" placeholder="Fecha inicio" />
+        </div>
+        <div className="form-group mb-2">
+          <label>Fecha y hora fin</label>
+          <input className="form-control" placeholder="Fecha inicio" />
+        </div>
         <hr />
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ipsa quasi quidem laborum incidunt quas saepe praesentium amet nisi illum?</p>
+        <div className="form-group mb-2">
+          <label>Titulo y notas</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Título del evento"
+            name="title"
+            autoComplete="off"
+            value={formValues.title}
+            onChange={handleInputChange}
+          />
+          <small id="emailHelp" className="form-text text-muted">
+            Una descripción corta
+          </small>
+        </div>
+        <div className="form-group mb-2">
+          <textarea
+            type="text"
+            className="form-control"
+            placeholder="Notas"
+            rows="5"
+            name="notes"
+            value={formValues.notes}
+            onChange={handleInputChange}
+          ></textarea>
+          <small id="emailHelp" className="form-text text-muted">
+            Información adicional
+          </small>
+        </div>
+
+        <button type="submit" className="btn btn-outline-primary btn-block">
+          <i className="far fa-save"></i>
+          <span> Guardar</span>
+        </button>
+      </form>
     </Modal>
-  )
-}
+  );
+};
